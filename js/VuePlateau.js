36810d5ea86cb7aa47a66f4d4b649c63380f2e5d
Benjamin Milhet <66473784+Orchanyne@users.noreply.github.com>
2022-09-06 17:43:28 +0200

    initialisation du plateau

---
+++ js/VuePlateau.js
@@ -0,0 +1,38 @@
+export class VuePlateau {
+
+    afficherPlateau() {
+        let emplacement = document.querySelector("playground"); // On recupere l'element HTML qui contient le plateau de jeu
+
+        let niveau = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]
+        ];
+
+        niveau.forEach((element) => { // On parcourt chaque ligne du plateau de jeu
+            let div = document.createElement("div"); // On cree une div qui va contenir la ligne
+            div.className = "ligne"; // On lui donne la classe ligne
+
+            element.forEach((donne) => { // On parcourt chaque case de la ligne
+                let minidiv = document.createElement("div"); // On cree une div qui va contenir la case
+                //let img = document.createElement("img"); // On cree une image qui sera dans la case
+
+                minidiv.className = "case"; // On lui donne la classe case
+                minidiv.style.display = 'inline-block'; // On lui donne la propriete display
+                minidiv.innerHTML = donne;
+                //img.className = "imgCase"; // On lui donne la classe imgCase
+
+                //if (donne != "V") img.src = '../images/' + donne + '.png'; // On lui donne l'image correspondante
+
+                //minidiv.appendChild(img); // On ajoute l'image dans la case
+                div.appendChild(minidiv); // On ajoute la case dans la ligne
+
+            });
+
+            emplacement.appendChild(div); // On ajoute la ligne dans le plateau de jeu
+        });
+    }
+}
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var fs = require('fs');
+const express = require('express');
+const app = express();
 
-http.listen(process.env.PORT || 1825, function() {
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const fs = require('fs');
+const port = 1825;
+
+app.use(express.static('public'));
+app.use("/style/style.css", express.static(__dirname + '/style/style.css'));
+app.use("/js/client.js", express.static(__dirname + '/js/client.js'));
+app.use("/js/VuePlateau.js", express.static(__dirname + '/js/VuePlateau.js'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/templates/index.html');
+});
+
+app.get('/games/game-lobby', (req, res) => {
+    res.sendFile(__dirname + '/templates/games/game-lobby.html');
+});
+
+http.listen(port, () => {
     console.log("Server running !");
 });
